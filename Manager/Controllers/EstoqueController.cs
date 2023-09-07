bccb19de5546441556ae1c9eab8637e6cd003acd
GabrielMatheus00 <gabriel.matheus00@hotmail.com>
2023-09-06 22:35:07 -0300

    Começando o endpoint de salvar produtos

--- Manager/Controllers/EstoqueController.cs
+++ Manager/Controllers/EstoqueController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Manager.Models;
 using Microsoft.Extensions.WebEncoders.Testing;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace Manager.Controllers
 {
@@ -18,15 +19,50 @@ namespace Manager.Controllers
         {
             _context = context;
         }
-        public void TesteLeitura()
+        public void SaveProduto(FormCollection values)
         {
-            var teste = _context.Produtos.ToList();
+            try
+            {
+                bool newRecord = false;
+                Produto oProduto;
+                if (string.IsNullOrEmpty(values["ProdutoID"]))
+                {
+                    newRecord = true;
+                }
+                if (newRecord)
+                {
+                    oProduto = new Produto();
+                }
+                else
+                {
+                    oProduto = _context.Produtos.Where(f => f.Produtoid == Convert.ToInt32(values["ProdutoID"])).FirstOrDefault();
+                    if (oProduto == null) { throw new Exception("O Produto não foi encontrado!"); }
+                }
+                oProduto.Descricao = values["Descricao"];
+                oProduto.Fornecedor = values["Fornecedor"];
+                if (string.IsNullOrEmpty(values["Preco"]) 
+                    || !(int.TryParse(values["Preco"], out int a)))
+                {
+                    throw new Exception("Preço inválido!");
+                }
+                if (string.IsNullOrEmpty(values["Quantidade"]))
+                {
+                    oProduto.Quantidadeemestoque = 0;
+                }
+                else
+                {
+                    oProduto.Quantidadeemestoque = Convert.ToInt32(values["Quantidade"]);
+                }
+            }
+            catch
+            {
+
+            }
+
         }
         public void TesteAdicao()
         {
             
         }
-
-        
     }
 }
